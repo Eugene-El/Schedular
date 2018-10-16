9d306396ec3809129753dd1ea12ead95c9969181
El <lomashonok@inbox.lv>
2018-10-16 22:53:43 +0300

    Created web project Add calendar api controller

---
+++ SchedularWebApi/Controllers/CalendarController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchedularLib.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedularWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalendarController : Controller
+    {
+        private ICalendarFactory calendarFactory;
+
+        public CalendarController()
+        {
+            calendarFactory = new CalendarFactory();
+        }
+
+        [HttpGet]
+        public List<int> Test()
+        {
+            return new List<int>() { 1, 2, 3, 4 };
+        }
+    }
+}
